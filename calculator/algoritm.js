650fbdbfdd42f15bf8333542e5bd4f67e78c8971
STANISLAV <minolin27@gmail.com>
2017-02-28 13:03:32 +0300

    Update algoritm.js
    change alg calc

--- calculator/algoritm.js
+++ calculator/algoritm.js
@@ -141,49 +141,49 @@ if(t==="dot"&&dot===0&&l.length>0){
     }
   
 
-   if(t==="plus"||t==="minus"||t==="multiply"||t==="division"){
-    change(t);
-    operation="+"; 
+//    if(t==="plus"||t==="minus"||t==="multiply"||t==="division"){
+//     change(t);
+//     operation="+"; 
+//     num = Number(number);
+//     if(act===1){
+//       addN();
+//     }else if(act===0){
+//       actZero();
+//       }
+//     reset();
+//   }
+  if(t==="minus"){
+    operation="-";
     num = Number(number);
-    if(act===1){
+    if(act>0){
       addN();
-    }else if(act===0){
+    }else if(act<1){
       actZero();
       }
     reset();
   }
-  // if(t==="minus"){
-  //   operation="-";
-  //   num = Number(number);
-  //   if(act>0){
-  //     addN();
-  //   }else if(act<1){
-  //     actZero();
-  //     }
-  //   reset();
-  // }
 
-  // if(t==="multiply"){
-  //   operation="*";
-  //   num = Number(number);
-  //   if(act>0){
-  //     addN(); 
-  //   }else if(act<1){
-  //     actZero();
-  //   }
-  //   reset();
+  if(t==="multiply"){
+    operation="*";
+    num = Number(number);
+    if(act>0){
+      addN(); 
+    }else if(act<1){
+      actZero();
+    }
+    reset();
     
-  // }
-  // if(t==="division"){
-  //   operation="/";
-  //   num = Number(number);
-  //   if(act>0){
-  //      addN();  
-  //   }else if(act<1){
-  //     actZero();
-  //   }
-  //   reset();
-  // }
+  }
+  if(t==="division"){
+    operation="/";
+    num = Number(number);
+    if(act>0){
+       addN();  
+    }else if(act<1){
+      actZero();
+    }
+    reset();
+  }
 
   if(pl.length>3){
 
