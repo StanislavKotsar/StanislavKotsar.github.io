7922a88a8deed9f2879059cc570418980b22420a
STANISLAV <minolin27@gmail.com>
2017-03-04 15:52:39 +0300

    Change quote machine

--- quote/script.js
+++ quote/script.js
@@ -4,33 +4,25 @@ $(document).ready(function() {
   $.ajaxSetup({ cache: false }); 
   
  $("#getMessage").on("click", function (){
-      
-  $.getJSON("https://www.quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback="  + new Date().getTime(), function(json){
-     
-
+  
+      $("#content").fadeOut(1000, function(){
+        $.getJSON("https://www.quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback="  + new Date().getTime(), function(json){
 var val =json[0]; 
-var content =val.content;
-    
-               
-
-     $('#quote').html( val.content);
-    document.getElementById("quote").style.font= "'Oleo Script', cursive";
-    
+var content =val.content;      
    if(content.length> 140&&content.length<220){ document.getElementById("quote").style.font= " 30px 'Oleo Script', cursive";
 document.getElementById("tweet").disabled = true;                 
     }else if(content.length<= 140){document.getElementById("quote").style.font= " 35px 'Oleo Script', cursive";
 document.getElementById("tweet").disabled = false;                            
-                                  }else{
+    }else{
       document.getElementById("quote").style.font= " 20px 'Oleo Script', cursive";
       document.getElementById("tweet").disabled = true;
     } 
-    
-    
-    
- 
+    $('#quote').html(content);
     $('#author').html( val.title );
+    $("#content").fadeIn(500);
     
-
+      });
+      
   });
 
  });
