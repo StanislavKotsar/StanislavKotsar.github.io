0ee4dcf7b5f319437859fc5f71dbbfc07182f8ac
STANISLAV <minolin27@gmail.com>
2017-04-23 20:06:53 +0300

    Revert "Add buttons,"
    This reverts commit 30960c8ce847e518af22cef3539edacdd7ee1cc0.

--- script.js
+++ script.js
@@ -11,30 +11,7 @@ $('a[href*="#"]:not([href="#"])').click(function() {
   }
 });
 
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
 var today = new Date();
 var year = today.getFullYear();
+
 document.getElementById("date").textContent = year;
-
-jQuery(function( $ ){ //Prevent conflict - http://snipplr.com/view/43906/
-
-// Back to Top
-	var scrollDiv = document.createElement("div");
-	jQuery(scrollDiv).attr("id", "toTop").attr("data-toggle", "tooltip").attr("data-placement", "left").attr("title", "Back to Top").html("<i class='fa  fa-chevron-up'></i>").appendTo("body");
-	jQuery(window).scroll(function () {
-		if (jQuery(this).scrollTop() > 700) {
-			jQuery("#toTop").fadeIn();
-		} else {
-			jQuery("#toTop").fadeOut();
-		}
-	});
-	jQuery("#toTop").click(function () {
-		jQuery("body,html").animate({
-			scrollTop: 0
-		},
-		800);
-	});
-  });
